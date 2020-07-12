25914c7bd350e0879ec4fc39cdb6d4b4d78c79a9
Analia Mok <amok@geekhive.com>
2020-07-12 14:18:47 -0400

    Setup autoformatting. Added flavor profile relationship to coffee

--- api/coffee/controllers/coffee.js
+++ api/coffee/controllers/coffee.js
@@ -1,8 +1,53 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve records.
+   * @param {Array} ctx
+   */
+  async find(ctx) {
+    let entities;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.coffee.search(ctx.query);
+    } else {
+      entities = await strapi.services.coffee.find(ctx.query);
+    }
+
+    // Clean up returned relationship fields.
+    return entities.map((entity) => {
+      const coffee = sanitizeEntity(entity, { model: strapi.models.coffee });
+
+      if (coffee.roast_level && coffee.roast_level.name) {
+        const roastLevel = coffee.roast_level.name;
+
+        delete coffee.roast_level;
+
+        coffee.roast_level = roastLevel;
+      }
+
+      if (coffee.flavor_profile) {
+        const flavor_profile = {};
+        if (coffee.flavor_profile.name) {
+          flavor_profile['name'] = coffee.flavor_profile.name;
+        }
+
+        if (coffee.flavor_profile.brief_description) {
+          flavor_profile['brief_description'] =
+            coffee.flavor_profile.brief_description;
+        }
+
+        delete coffee.flavor_profile;
+
+        coffee.flavor_profile = flavor_profile;
+      }
+
+      return coffee;
+    });
+  },
+};
